8ff7e35750d02517f19fe3cb2a5c515370de0723
SoundBrake <mesuper222@gmail.com>
2018-07-16 17:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file.
+function lowerCaseStudentNames(array){
+  var arr = array.slice()
+  for(var x = 0; x < array.length; x++){
+    arr[x] = array[x].toLowerCase()
+  }
+  return arr
+}
+
+function nameToAttributes(array){
+  var 
+}
+
